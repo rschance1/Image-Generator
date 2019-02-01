95a56099246d40fb35684d7ed8b3a1da9d24c226
rschance1 <richardchance1980@gmal.com>
2019-01-31 22:18:41 -0600

    Loading sample image generator mobile app

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/src/app.dart';
+
+//Define a 'main' function to tun when our app starts
+void main() {
+  runApp(App());
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,52 @@
+//import flutter helper library
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+/*
+Create a class that will be our custom widget
+This class must extend 'StatelessWidget' base class
+*/
+class App extends StatefulWidget {
+createState() {
+  return AppState();
+}
+}
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter ++;
+     final response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+     final imageModel = ImageModel.fromJson(json.decode(response.body));
+     
+     setState(() {
+            images.add(imageModel);
+          });
+     
+     
+  }
+/*
+Must define a build method that returns
+the widget that 'this' widget will show
+*/
+@override
+  Widget build(BuildContext context) {
+   
+    return MaterialApp(
+    home: Scaffold(
+      body: ImageList(images),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: fetchImage,
+       
+      ),
+      appBar: AppBar(
+        title: Text('Lets see some images'),
+      ),
+    ),
+  );
+  }
+}
